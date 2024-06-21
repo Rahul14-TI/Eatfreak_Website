927360e6877838eebf9f077075ca049cf818daf6
Rahul Tiwari <tiwari.rahul1403@gmail.com>
2024-06-21 11:20:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const slides = document.querySelectorAll(".slide")
+var counter = 0;
+ console.log(slides)
+
+slides.forEach(
+    (slide, index) => {
+        slide.style.left = `${index * 100}%`
+    }
+)
+
+const goPrev = () => {
+    counter--
+    slideimage()
+}
+
+
+const goNext = () => {
+    counter++
+    slideimage()
+}
+
+// function autoslider(){
+//     deletInterval = setInterval(timer, 10000);
+//     function timer(){
+//         goNext();
+//     }
+// }
+// autoslider();
+
+const slideimage = () =>{
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 100}%)`
+        }
+    )
+}
